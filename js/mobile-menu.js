5d0c64b8536a7cdc93ff7e73e028e01bb806dbc9
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-05-26 15:29:00 +0200

    Added mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,19 @@
+(() => {
+  const menuRefs = {
+    menuBtn: document.querySelector('[data-menu-button]'),
+    menu: document.querySelector('[data-menu]'),
+    menuLink: document.querySelectorAll('[data-menu-link]'),
+  };
+
+  menuRefs.menuBtn.addEventListener('click', toggleMenu);
+  menuRefs.menuLink.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    const expanded = menuRefs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
+    menuRefs.menuBtn.setAttribute('aria-expanded', !expanded);
+
+    menuRefs.menuBtn.classList.toggle('is-open');
+    menuRefs.menu.classList.toggle('is-open');
+    document.body.classList.toggle('modal-is-open');
+  }
+})();
